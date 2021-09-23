07ec226bcb31bb49a2c10e9313d5bce28d0b1abf
shabashab <shabashab.04@gmail.com>
2021-09-23 08:46:34 +0300

    Add a lesson from 16-09-21

---
+++ 16-09-21/Homework/src/edu/naukma/hw1/Program.java
@@ -0,0 +1,11 @@
+package edu.naukma.hw1;
+
+public class Program {
+  public static void main(String[] args) {
+    System.out.println("Символьний: це або 1 символ Unicode, взятий в одинарні лапки");
+    System.out.println("(char C=‘!’) або послідовність, яка починається з \\: (\\n – кінець");
+    System.out.println("рядка; \\t – табуляція; \\’ – одинарні лапки; \\” – подвійні лапки;");
+    System.out.println("\\\\ – зворотній слеш); літерал може задаватися Unicode-кодом,");
+    System.out.println("напр. \\u0041 (це літера А);");
+  }
+}
