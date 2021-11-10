6755b45957685c0b82c9915597b629765e45850c
Artem Tarasenko <shabashab.04@gmail.com>
2021-11-10 09:41:12 +0200

    [11-11-21-hw]: implement input checks for task 1

--- 11-11-21-hw/src/edu/ukma/tarasenko/Pythagorean.java
+++ 11-11-21-hw/src/edu/ukma/tarasenko/Pythagorean.java
@@ -6,14 +6,19 @@ public class Pythagorean {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     double a, b, c;
 
     System.out.println("Enter value to compute Pythagorean theorem.");
+
     System.out.print("a: ");
     a = scanner.nextInt();
+    if(a <= 0)
+      throw new IllegalArgumentException("Invalid input data. a should be more than 0");
+
     System.out.print("b: ");
     b = scanner.nextInt();
+    if(b <= 0)
+      throw new IllegalArgumentException("Invalid input data. b should be more than 0");
 
     c = Math.sqrt(a*a + b*b);
     System.out.println("c = " + c);
