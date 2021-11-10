e60575d8fefd7b0cab9c994dccc8b7d629d053ff
Artem Tarasenko <shabashab.04@gmail.com>
2021-11-10 09:51:08 +0200

    [11-11-21-hw]: implement task 2

---
+++ 11-11-21-hw/src/edu/ukma/tarasenko/MinMax.java
@@ -0,0 +1,64 @@
+package edu.ukma.tarasenko;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class MinMax {
+  public static int[] input() {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Integer> result = new ArrayList<>();
+
+    while (true) {
+      System.out.print("? ");
+      int value = scanner.nextInt();
+
+      if (value == 0)
+        break;
+
+      result.add(value);
+    }
+
+    return result.stream().flatMapToInt(IntStream::of).toArray();
+  }
+
+  public static int min(int[] value) {
+    if (value.length == 0)
+      throw new IllegalArgumentException();
+
+    int minValue = value[0];
+    for (int i = 1; i < value.length; i++)
+      if (value[i] < minValue)
+        minValue = value[i];
+
+    return minValue;
+  }
+
+  public static int max(int[] value) {
+    if(value.length == 0)
+      throw new IllegalArgumentException();
+
+    int maxValue = value[0];
+    for(int i = 1; i < value.length; i++)
+      if(value[i] > maxValue)
+        maxValue = value[i];
+
+    return maxValue;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("This program finds the largest and the smallest numbers");
+
+    int[] input = input();
+
+    if (input.length == 0) {
+      System.out.println("No input data is provided");
+      return;
+    }
+
+    int min = min(input);
+    int max = max(input);
+    System.out.printf("smallest: %d\n", min);
+    System.out.printf("largest: %d\n", max);
+  }
+}
