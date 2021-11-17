152821addf0aec402bc2245d7f247a92c73c5afe
Artem Tarasenko <shabashab.04@gmail.com>
2021-11-17 12:03:10 +0200

    [18-12-21-hw]: implement task 1

---
+++ 18-12-21-hw/src/edu/ukma/tarasenko/GuessTheNumber.java
@@ -0,0 +1,31 @@
+package edu.ukma.tarasenko;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GuessTheNumber {
+  static final int MIN_NUMBER = 1;
+  static final int MAX_NUMBER = 1000;
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int number = ThreadLocalRandom.current().nextInt(MIN_NUMBER, MAX_NUMBER);
+    System.out.printf("Я вибрав число між %d та %d\n", MIN_NUMBER, MAX_NUMBER);
+
+    while(true) {
+      System.out.print("Ваш варіант: ");
+      int input = scanner.nextInt();
+
+      if(input == number)
+        break;
+
+      if(input > number)
+        System.out.println("Менше");
+      else
+        System.out.println("Більше");
+    }
+
+    System.out.println("Вірно!");
+  }
+}
