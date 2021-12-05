47c10a68bbeeb06ef7d5c58590ddd40ae2d4f69a
Artem Tarasenko <shabashab.04@gmail.com>
2021-12-05 02:21:03 +0200

    [08-12-21-hw]: fix task 2

--- 08-12-21-hw/src/edu/ukma/tarasenko/Backup.java
+++ 08-12-21-hw/src/edu/ukma/tarasenko/Backup.java
@@ -1,39 +1,9 @@
 package edu.ukma.tarasenko;
 
-import javax.management.BadStringOperationException;
 import java.io.*;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Backup {
-  private static void fatalError(String message) {
-    System.out.printf("ERROR: %s\n", message);
-    System.exit(1);
-  }
-
-  private static String replaceLast(String input, String toReplace, String replacement) {
-    return input.replaceFirst("(" + toReplace + ")$", replacement);
-  }
-
-  private static String createOutputFilePath(String inputFileName) throws BadStringOperationException {
-    String regex = "([^\\/\\\\\\.]*)(\\.([a-zA-Z]+))?$";
-    Pattern pattern = Pattern.compile(regex);
-
-    Matcher matcher = pattern.matcher(inputFileName);
-
-    if(matcher.find()) {
-      String extension = matcher.group(2);
-
-      if(extension == null)
-        extension = "";
-
-      return replaceLast(inputFileName, extension, ".bak");
-    }
-
-    throw new BadStringOperationException("Invalid input string");
-  }
-
   private static void copyFile(File inputFile, File outputFile) throws IOException {
     final int BUFFER_SIZE = 1024;
 
@@ -43,7 +13,7 @@ public class Backup {
     byte[] buffer = new byte[BUFFER_SIZE];
     int readCount;
 
-    while((readCount = inputFileStream.read(buffer)) > 0) {
+    while ((readCount = inputFileStream.read(buffer)) > 0) {
       outputFileStream.write(buffer, 0, readCount);
     }
 
@@ -58,20 +28,13 @@ public class Backup {
     String fileName = scanner.nextLine();
 
     File inputFile = new File(fileName);
-
-    String outputFilePath = "";
-    try {
-      outputFilePath = createOutputFilePath(fileName);
-    } catch (BadStringOperationException exception) {
-      fatalError(exception.getMessage());
-    }
-
-    File outputFile = new File(outputFilePath);
+    File outputFile = new File(fileName + ".bak");
 
     try {
       copyFile(inputFile, outputFile);
     } catch (IOException exception) {
-      fatalError("An error occurred when trying to copy a file");
+      System.out.printf("ERROR: %s\n", exception.getMessage());
+      System.exit(1);
     }
   }
 }
