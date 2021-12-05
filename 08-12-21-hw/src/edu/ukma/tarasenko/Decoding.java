59d7e41eda8b6456a9f12c3919b096d88d8edb24
Artem Tarasenko <shabashab.04@gmail.com>
2021-12-05 02:10:54 +0200

    [08-12-21-hw]: implement task 4 and 5

---
+++ 08-12-21-hw/src/edu/ukma/tarasenko/Decoding.java
@@ -0,0 +1,58 @@
+package edu.ukma.tarasenko;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Decoding {
+
+  private static void writeFile(File file, String contents) throws IOException {
+    FileWriter fileWriter = new FileWriter(file);
+    fileWriter.write(contents);
+    fileWriter.close();
+  }
+
+  private static String decodeString(String input, int shift) {
+    StringBuilder outputBuilder = new StringBuilder();
+
+    for(int i = 0; i < input.length(); i++) {
+      outputBuilder.append(input.charAt(i) - shift);
+    }
+
+    return outputBuilder.toString();
+  }
+
+  private static void decodeFile(File file, int shift) throws IOException {
+    Scanner fileScanner = new Scanner(file);
+    StringBuilder outputBuilder = new StringBuilder();
+
+    while(fileScanner.hasNextLine())  {
+      String line = fileScanner.nextLine();
+      String encodedLine = decodeString(line, shift);
+      outputBuilder.append(encodedLine);
+    }
+
+    String output = outputBuilder.toString();
+    writeFile(file, output);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the name of the file you want to decode: ");
+    String fileName = scanner.nextLine();
+
+    System.out.print("Enter the shift: ");
+    int shift = scanner.nextInt();
+
+    File inputFile = new File(fileName);
+
+    try {
+      decodeFile(inputFile, shift);
+    } catch (IOException e) {
+      System.out.println("ERROR: " + e.getMessage());
+      System.exit(1);
+    }
+  }
+}
---
+++ 08-12-21-hw/src/edu/ukma/tarasenko/Encoding.java
@@ -0,0 +1,55 @@
+package edu.ukma.tarasenko;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Encoding {
+  private static String encodeString(String input, int shift) {
+    StringBuilder outputStringBuilder = new StringBuilder();
+
+    for(int i = 0; i < input.length(); i++) {
+      outputStringBuilder.append(input.charAt(i) + shift);
+    }
+
+    return outputStringBuilder.toString();
+  }
+
+  private static void writeFile(File file, String contents) throws IOException {
+    FileWriter fileWriter = new FileWriter(file);
+    fileWriter.write(contents);
+    fileWriter.close();
+  }
+
+  private static void encodeFile(File fileToEncode, int shift) throws IOException {
+    Scanner fileScanner = new Scanner(fileToEncode);
+    StringBuilder outputBuilder = new StringBuilder();
+
+    while(fileScanner.hasNextLine())  {
+      String line = fileScanner.nextLine();
+      String encodedLine = encodeString(line, shift);
+      outputBuilder.append(encodedLine);
+    }
+
+    String output = outputBuilder.toString();
+    writeFile(fileToEncode, output);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the name of the file you want to encode: ");
+    String fileName = scanner.nextLine();
+
+    System.out.print("Enter the shift: ");
+    int shift = scanner.nextInt();
+
+    File inputFile = new File(fileName);
+
+    try {
+      encodeFile(inputFile, shift);
+    } catch (IOException e) {
+      System.out.println("ERROR: " + e.getMessage());
+      System.exit(1);
+    }
+  }
+}
